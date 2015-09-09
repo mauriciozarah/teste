45103667ca09e414cf0b43f138fcafd4934d8a09
mauricio <mauricio@hotmail.com>
2015-09-09 07:19:51 -0300

    modificado a ci para null

--- funcoes.php
+++ funcoes.php
@@ -1,7 +1,7 @@
 <?php
 
 if(!function_exists("carregaHtml")):
-	function carregaHtml($pg="home", $CI, $data=null){
+	function carregaHtml($pg="home", $CI=null, $data=null){
 		if($CI):
 			$CI->parser->parse($pg, $data);
 		endif;
